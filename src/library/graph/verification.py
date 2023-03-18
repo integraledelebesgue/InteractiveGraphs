323dc96457b6cf48cf300173aeb9eab578d92fd7
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-03-18 19:45:02 +0100

    Graph property checking decorators

--- src/library/graph/verification.py
+++ src/library/graph/verification.py
@@ -5,7 +5,7 @@ from numpy.typing import NDArray
 from src.library.graph.representations import list_to_matrix
 
 
-def verify(graph_constructor: Callable) -> Callable:
+def verify_args(graph_constructor: Callable) -> Callable:
     def verifier(
             self, *,
             adj_list: Optional[NDArray] = None,
@@ -31,10 +31,50 @@ def verify(graph_constructor: Callable) -> Callable:
 
         return graph_constructor(
             self,
-            adjacency_list=adj_list,
-            adjacency_matrix=adj_matrix,
+            adj_list=adj_list,
+            adj_matrix=adj_matrix,
             weighted=weighted,
             directed=directed
         )
+
+    return verifier
+
+
+def directed_only(graph_func: Callable) -> Callable:
+    def verifier(graph, *args, **kwargs):
+        if not graph.directed:
+            raise AttributeError(f"Function {graph_func} cannot be performed on an undirected graph")
+
+        return graph_func(graph, *args, **kwargs)
+
+    return verifier
+
+
+def undirected_only(graph_func: Callable) -> Callable:
+    def verifier(graph, *args, **kwargs):
+        if graph.directed:
+            raise AttributeError(f"Function {graph_func} cannot be performed on a directed graph")
+
+        return graph_func(graph, *args, **kwargs)
+
+    return verifier
+
+
+def weighted_only(graph_func: Callable) -> Callable:
+    def verifier(graph, *args, **kwargs):
+        if not graph.weighted:
+            raise AttributeError(f"Function {graph_func} cannot be performed on a non-weighted graph")
+
+        return graph_func(graph, *args, **kwargs)
+
+    return verifier
+
+
+def positive_weights(graph_func: Callable) -> Callable:
+    def verifier(graph, *args, **kwargs):
+        if not np.all(graph.adj_matrix > 0):
+            raise AttributeError(f"Function {graph_func} cannot be performed on a graph with non-positive weights")
+
+        return graph_func(graph, *args, **kwargs)
 
     return verifier
