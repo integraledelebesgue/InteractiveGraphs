5320fc027578fa39d88d66fa38f3e131ceabcc45
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-03-19 22:38:42 +0100

    Floyd-Warshall fix

--- src/library/algorithms/minimal_paths/floyd_warshall.py
+++ src/library/algorithms/minimal_paths/floyd_warshall.py
@@ -1,21 +1,25 @@
-from copy import deepcopy
-
 from numpy.typing import NDArray
 
 from src.library.graph.graph import Graph
 from src.library.graph.verification import weighted_only, positive_weights
+
+inf = float('inf')
 
 
 @weighted_only
 @positive_weights
 def floyd_warshall(graph: Graph) -> NDArray:
     n = graph.order
-    distance = deepcopy(graph.adj_matrix)
-    distance[distance == 0] = float('inf')
+
+    distance = graph.adj_matrix\
+        .copy()\
+        .astype(float)
+
+    distance[distance == graph.null_weight] = inf
 
     for u in range(n):
-        for v in range(n):
-            for w in range(n):
+        for v in filter(lambda x: x != u, range(n)):
+            for w in filter(lambda x: x != u and x != v, range(n)):
                 distance[u, v] = min(
                     distance[u, v],
                     distance[u, w] + distance[w, v]
