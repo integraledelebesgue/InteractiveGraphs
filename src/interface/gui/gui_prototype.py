23bf3abf5b360394b7cd3ddfd57fe4e86b2a3be2
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-04-01 17:54:10 +0200

    Load/Save UI buttons, context menu

--- src/interface/gui/gui_prototype.py
+++ src/interface/gui/gui_prototype.py
@@ -1,12 +1,12 @@
 import threading
 from dataclasses import dataclass
+from os import getcwd
 
 import numpy as np
 import pygame
 import pygame_gui
 
-from src.library.graph.graph import Graph
-from src.library.graph.prototype_graph_view import PrototypeGraphView
+from src.library.graph.graph import Graph, GraphView
 
 window_size = (1600, 1200)
 
@@ -43,10 +43,16 @@ class Position:
 
 @dataclass
 class ContextMenu:
-    position: Position
-    background: pygame.Rect
+    active: bool
+    container: pygame_gui.core.UIContainer
     row_height: int
     items: list[pygame_gui.elements.UIButton]
+
+
+@dataclass
+class GraphIO:
+    input_box: pygame_gui.elements.UITextEntryLine
+    active: bool
 
 
 class App(threading.Thread):
@@ -73,8 +79,6 @@ class App(threading.Thread):
         self.top_margin = 15
         self.bottom_margin = 15
 
-        self.graph_view = test_graph.view()
-
         self.graph_area_corner = (self.left_margin + 120, self.top_margin)
         self.graph_area_size = (1445, 1170)
 
@@ -82,6 +86,8 @@ class App(threading.Thread):
             self.graph_area_corner,
             self.graph_area_size
         )
+
+        self.graph_view = test_graph.view(self.graph_area_size)
 
         self.draggable = []
         self.dragging_allowed = True
@@ -91,45 +97,99 @@ class App(threading.Thread):
         self.mouse_x = 0
         self.mouse_y = 0
 
+        self.load_io = GraphIO(
+            input_box=pygame_gui.elements.UITextEntryLine(
+                relative_rect=pygame.Rect((self.left_margin + 120, self.top_margin), (1000, 30)),
+                initial_text=getcwd(),
+                manager=self.manager,
+                visible=False
+            ),
+            active=False
+        )
+
+        def load_graph_handler(_e):
+            if not self.load_io.input_box.visible and not self.save_io.active:
+                self.load_io.input_box.visible = True
+                self.load_io.active = True
+
+        _load_graph = pygame_gui.elements \
+            .UIButton(
+                relative_rect=pygame.Rect((self.left_margin, self.top_margin), (100, 50)),
+                text='Load',
+                manager=self.manager,
+                tool_tip_text='Load new graph from file and display in editor'
+            )\
+            .set_handler(load_graph_handler)
+
+        self.save_io = GraphIO(
+            input_box=pygame_gui.elements.UITextEntryLine(
+                relative_rect=pygame.Rect((self.left_margin + 120, self.top_margin + 65), (1000, 30)),
+                initial_text=getcwd(),
+                manager=self.manager,
+                visible=False
+            ),
+            active=False
+        )
+
+        def save_graph_handler(_e):
+            if not self.save_io.input_box.visible and not self.save_io.active:
+                self.save_io.input_box.visible = True
+                self.save_io.active = True
+
+        _save_graph = pygame_gui.elements \
+            .UIButton(
+                relative_rect=pygame.Rect((self.left_margin, self.top_margin + 65), (100, 50)),
+                text='Save',
+                manager=self.manager,
+                tool_tip_text='Save current graph to file'
+            )\
+            .set_handler(save_graph_handler)
+
+        context_menu_button_size = (200, 25)
+
         self.context_menu = ContextMenu(
-            position=Position(-100, -100),
-            background=(back := pygame.Rect((-100, -100), (100, 150))),
+            container=(cont := pygame_gui.core.UIContainer(
+                pygame.Rect(
+                    (-200, -200),
+                    (200, 125),
+                ),
+                manager=self.manager
+            )),
             row_height=50,
             items=[
                 pygame_gui.elements.UIButton(
-                    relative_rect=pygame.Rect((back.x, back.y), (100, 50)),
-                    text='Button1',
+                    relative_rect=pygame.Rect((0, 0), context_menu_button_size),
+                    text='Add node',
                     manager=self.manager,
+                    container=cont
                 ),
                 pygame_gui.elements.UIButton(
-                    relative_rect=pygame.Rect((back.x, back.y + 50), (100, 50)),
-                    text='Button2',
+                    relative_rect=pygame.Rect((0, 25), context_menu_button_size),
+                    text='Delete node',
                     manager=self.manager,
+                    container=cont
                 ),
                 pygame_gui.elements.UIButton(
-                    relative_rect=pygame.Rect((back.x, back.y + 100), (100, 50)),
-                    text='Button1',
+                    relative_rect=pygame.Rect((0, 50), context_menu_button_size),
+                    text='Add edge',
                     manager=self.manager,
+                    container=cont
+                ),
+                pygame_gui.elements.UIButton(
+                    relative_rect=pygame.Rect((0, 75), context_menu_button_size),
+                    text='Delete edge',
+                    manager=self.manager,
+                    container=cont
+                ),
+                pygame_gui.elements.UIButton(
+                    relative_rect=pygame.Rect((0, 100), context_menu_button_size),
+                    text='Change weight',
+                    manager=self.manager,
+                    container=cont
                 )
-            ]
+            ],
+            active=False
         )
-
-        self.context_menu_active = False
-
-        self.init_ui()
-
-    def init_ui(self):
-        def new_graph_button_handler(_e):
-            print('Hello')
-
-        _new_graph_button = pygame_gui.elements \
-            .UIButton(
-                relative_rect=pygame.Rect((self.left_margin, self.top_margin), (100, 50)),
-                text='Hello',
-                manager=self.manager,
-                tool_tip_text='Print \'Hello\' in console'
-            )\
-            .set_handler(new_graph_button_handler)
 
     def run(self):
         self.running = True
@@ -141,11 +201,31 @@ class App(threading.Thread):
                     case pygame.QUIT:
                         self.running = False
 
-                    case pygame_gui.UI_BUTTON_PRESSED:
-                        try: event.ui_element.handler(event)
-                        finally: pass
+                    # UI Button handling
 
-                    # Node dragging part:
+                    case pygame_gui.UI_BUTTON_PRESSED \
+                            if hasattr(event.ui_element, 'handler'):
+                        event.ui_element.handler(event)
+
+                    # File I/O
+
+                    case pygame.KEYDOWN \
+                        if event.key == pygame.K_RETURN \
+                            and self.load_io.active:
+
+                        self.load_io.active = False
+                        self.load_io.input_box.visible = False
+                        self.load_graph(self.load_io.input_box.get_text())
+
+                    case pygame.KEYDOWN \
+                        if event.key == pygame.K_RETURN \
+                           and self.save_io.active:
+
+                        self.save_io.active = False
+                        self.save_io.input_box.visible = False
+                        self.save_graph(self.save_io.input_box.get_text())
+
+                    # Node dragging
 
                     case pygame.MOUSEBUTTONDOWN \
                         if event.button == 1 \
@@ -172,21 +252,21 @@ class App(threading.Thread):
                     case pygame.MOUSEMOTION:
                         self.mouse_x, self.mouse_y = event.pos
 
-                    # Context menu part:
+                    # Context menu
 
                     case pygame.MOUSEBUTTONDOWN \
                         if event.button == 3 \
-                            and not self.context_menu_active\
+                            and not self.context_menu.active\
                             and self.graph_area.collidepoint(event.pos) \
                             and not self.node_collision(event.pos):
 
                         self.open_context_menu(event.pos)
-                        self.context_menu_active = True
+                        self.context_menu.active = True
 
                     case pygame.MOUSEBUTTONDOWN \
-                            if self.context_menu_active:
+                            if self.context_menu.active:
                         self.close_context_menu()
-                        self.context_menu_active = False
+                        self.context_menu.active = False
 
                 self.manager.process_events(event)
 
@@ -205,15 +285,10 @@ class App(threading.Thread):
         quit()
 
     def open_context_menu(self, position):
-        print(f"Opening context menu on {position}")
-        self.context_menu.position = position
-        self.context_menu.background.x, self.context_menu.background.y = position
-        for i, item in enumerate(self.context_menu.items):
-            item.relative_rect.y = position[1] + i * self.context_menu.row_height
-            item.relative_rect.x = position[0]
+        self.context_menu.container.set_position(position)
 
     def close_context_menu(self):
-        self.open_context_menu((-100, -100))
+        self.open_context_menu((-200, -200))
 
     def draggable_collision(self, position):
         return next(
@@ -259,6 +334,12 @@ class App(threading.Thread):
                 15,
                 3
             )
+
+    def load_graph(self, filepath: str):
+        self.graph_view = GraphView.from_file(filepath)
+
+    def save_graph(self, filepath: str):
+        self.graph_view.to_file(filepath)
 
 
 if __name__ == '__main__':
