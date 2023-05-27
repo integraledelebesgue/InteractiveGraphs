189f505e80b74b5c0bd5ae8b95e45b1a4e106046
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-05-27 23:21:47 +0200

    Typing improvement

--- src/library/graph/representations.py
+++ src/library/graph/representations.py
@@ -2,11 +2,11 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def matrix_to_list(adjacency_matrix: NDArray, null_weight: int) -> list[NDArray]:
+def matrix_to_list(adjacency_matrix: np.ndarray[int], null_weight: int) -> list[np.ndarray[int]]:
     return [np.where(row != null_weight)[0] for row in adjacency_matrix]
 
 
-def list_to_matrix(adjacency_list: list[NDArray], null_weight: int) -> NDArray:
+def list_to_matrix(adjacency_list: list[np.ndarray[int]], null_weight: int) -> np.ndarray[int]:
     matrix = np.zeros((len(adjacency_list), len(adjacency_list)), int)
     matrix.fill(null_weight)
 
