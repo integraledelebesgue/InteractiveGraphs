c2ee08d61f97539aef4f14c964dcacc544de35a3
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-05-28 13:10:38 +0200

    Algorithm logic improvement, working Numba compilation

--- src/library/algorithms/transformations/conjugate.py
+++ src/library/algorithms/transformations/conjugate.py
@@ -1,15 +1,16 @@
+import numba
 import numpy as np
 
 from src.library.graph.graph import Graph
 
 
+@numba.jit(nopython=False, forceobj=True)
 def conjugate(graph: Graph) -> Graph:
     null_weight = graph.null_weight
 
-    conjugate_matrix = np.vstack(
-        [list(map(lambda weight: 1 if weight == null_weight else null_weight, row))
-         for row in graph.adj_matrix]
-    )
+    conjugate_matrix = graph.adj_matrix.copy()
+    conjugate_matrix[graph.adj_matrix == null_weight] = 1
+    conjugate_matrix[graph.adj_matrix != null_weight] = null_weight
 
     np.fill_diagonal(conjugate_matrix, -1)
 
