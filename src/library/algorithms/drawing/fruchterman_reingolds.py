55fea8bc6a1838cba4ff223cb76d2d1ad0c857e4
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-06-19 23:37:19 +0200

    ChatGPT proposal - doesn't work...

--- src/library/algorithms/drawing/fruchterman_reingolds.py
+++ src/library/algorithms/drawing/fruchterman_reingolds.py
@@ -1,48 +1,52 @@
+import math
+
 import numpy as np
-from numpy.typing import NDArray
 
 
-def repulsive_force(u, v, ideal_length) -> NDArray:
-    diff = u.position - v.position
-    print(u.position)
-    print(v.position)
+def distribute(graph, k=0.1, iterations=50):
+    # Initialize node positions
+    n = graph.order
+    positions = np.random.rand(n, 2)
 
-    if not (u.position[0] <= 0 or u.position[0] > 0):
-        exit(1)
+    # Set the area of the graph
+    area = math.sqrt(len(graph))
 
-    if not (v.position[0] <= 0 or v.position[0] > 0):
-        exit(1)
+    # Calculate initial distances between nodes
+    distances = np.linalg.norm(positions[:, None] - positions, axis=-1)
+    np.fill_diagonal(distances, 1e-5)  # Avoid division by zero
 
-    return diff / np.dot(diff, diff) * ideal_length ** 2
+    # Perform the main iterations
+    for _ in range(iterations):
+        # Calculate attractive forces
+        attractive_forces = np.zeros((n, n, 2))
+        for i in range(n):
+            for j in graph.neighbours(i):
+                delta = positions[i] - positions[j]
+                distance = distances[i, j]
+                attractive_forces[i, j] = (delta / distance) * (distance ** 2) / k
+                attractive_forces[j, i] = -attractive_forces[i, j]
 
+        # Calculate repulsive forces
+        repulsive_forces = np.zeros((n, n, 2))
+        for i in range(n):
+            for j in range(i + 1, n):
+                delta = positions[i] - positions[j]
+                distance = distances[i, j]
+                repulsive_forces[i, j] = (delta / distance) * (k ** 2) / distance
+                repulsive_forces[j, i] = -repulsive_forces[i, j]
 
-def attractive_force(u, v, ideal_length) -> NDArray:
-    diff = v.position - u.position
-    return np.sqrt(np.dot(diff, diff)) * diff / ideal_length
+        # Update node positions
+        delta_pos = np.sum(attractive_forces, axis=1) + np.sum(repulsive_forces, axis=1)
+        positions += delta_pos
 
+        # Limit the maximum displacement to the temperature (area) and cool down
+        displacement = np.linalg.norm(delta_pos, axis=1)
+        max_displacement = np.max(displacement)
+        if max_displacement > 0:
+            positions *= area / max_displacement
 
-def distribute_fruchterman_reingold(
-        graph_view,
-        ideal_length: int,
-        cooling_factor: float,
-        max_iter: int
-):
-    for i in range(max_iter):
-        node_forces = []
+    # Normalize node positions to fit within the drawing area
+    positions -= np.min(positions, axis=0)
+    positions /= np.max(positions, axis=0)
 
-        for vertex, node in graph_view.nodes.items():
-            node_forces.append(
-                np.sum(
-                    [repulsive_force(node, other_node, ideal_length)
-                     for other_node in graph_view.nodes.values() if other_node is not node]
-                ) +
-                np.sum(
-                    [attractive_force(node, neighbour, ideal_length)
-                     for neighbour in graph_view.neighbours(node) if neighbour is not node]
-                )
-            )
-
-        for node, force in zip(graph_view.nodes.values(), node_forces):
-            node.position += force * cooling_factor
-
-        cooling_factor **= i
+    return positions
